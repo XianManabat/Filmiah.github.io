56e19e5a1fbe7f72d44a68fa9606a6c470961fe4
XianManabat <120545134+XianManabat@users.noreply.github.com>
2025-05-30 09:40:08 +0800

    Add files via upload

---
+++ ViewModels/AdminLoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_web_app.ViewModels
+{
+    public class AdminLoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [EmailAddress]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        
+    }
+}
---
+++ ViewModels/Portfolio_inquiryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_web_app.ViewModels
+{
+    public class Portfolio_inquiryViewModel
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public required string Message { get; set; }
+    }
+}
